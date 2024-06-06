775efd71e2401cb921486214c564e70f49157994
Pragati <pragatibala15@gmail.com>
2024-06-06 23:04:55 +0530

    6-6

---
+++ TOPS-assignment&project/assessment/jQuery-assessment/slider.js
@@ -0,0 +1,49 @@
+
+      const leftBnt = document.getElementsByClassName("left")[0];
+      const rightBnt = document.getElementsByClassName("right")[0];
+      const sliderImg = document.getElementsByClassName("slider")[0].children;
+      const indicator = document.getElementsByClassName("indctr")
+      let sliderImgIndex = 1;
+      let indicatorIndex = 2;
+      indicator[2].style.backgroundColor="red";
+      leftBnt.addEventListener("click", (e) => {
+        e.preventDefault();
+        sliderImgIndex++;
+        indicatorIndex++
+        if (sliderImgIndex <= 4) {
+          slideLeft();
+          indicator[indicatorIndex].style.backgroundColor='red';
+        } else {
+          sliderImgIndex = 1;
+          indicatorIndex=0;
+        }
+      });
+      rightBnt.addEventListener("click", (e) => {
+        e.preventDefault();
+        sliderImgIndex--;
+        indicatorIndex--
+        if (sliderImgIndex >= 1) {
+          slideLeft();
+          indicator[indicatorIndex].style.backgroundColor='red';
+        } else {
+          sliderImgIndex = 5;
+          indicatorIndex=5;
+        }
+      });
+      function slideLeft() {
+        for (let i = 0; i < sliderImg.length; i++) {
+          indicator[i].style.backgroundColor='gray'
+          sliderImg[i].style.transform = `translateX(-${
+            sliderImgIndex * 500
+          }px)`;
+        }
+      }
+      function slideright() {
+        for (let i = 0; i < sliderImg.length; i++) {
+          indicator[i].style.backgroundColor='gray'
+          sliderImg[i].style.transform = `translateX(${
+            sliderImgIndex * 500
+          }px)`;
+        }
+      }
+    
